66fba6f3c691175a58ea7ca79899e4ee3f55a75c
Daniil <danilsasunov.2089@gmail.com>
2025-03-10 21:05:09 +0300

    learning

--- loader.s
+++ loader.s
@@ -1,3 +1,9 @@
+; it's a free small bootloader
+; goal of this - economy pc recourses
+; by replacing huge GRUB on this
+; and increase my own knowledges in this sphere
+; if you want you can copy this to your own project
+
 [ORG 0x7C00] 
 [BITS 16]
 
@@ -18,17 +24,50 @@ PRINT:
 	cmp al, 0
 	jne PRINT
 GDT:
-	LGDT gdt_param 
+	lgdt [gdt_start]
+	
+prot_mode:
+	xor cr0, cr0
+	mov 0x01, cr0
+
+init:
+	mov sp, 0x9C00
+	mov ax, 0x10
+	mov ds, ax
+	mov ss, ax
+	mov fs, ax
+	mov gs, ax
+	mov ex, ax
 
 	hlt 
 
-gdt_param:
-	dq 0x00000000 ; Base segment (8 zeroes)
-	dd 0x0000 ; Flags (4 zeroes)
-	dd 0x0000 ; Limit (4 zeroes)
-	db 0b10011010 ; Access byte
-	dq 0xFFFFFF ; Limit
+gdt_start:
+	; NULL descriptor
+	dd 0x00000000
+	dd 0x00000000
 
+	;KERNEL MODE:
+	
+	;code segment (0x8)
+	dw 0xFFFF	; Limit 
+	dw 0x0000	; Base
+	db 0x00 	; Base
+	db 0b10011010	; Access byte
+	db 0b11001111	; Flags
+	db 0x00		; Base
+
+	;data segment (0x10)
+	dw 0xFFFF	; Limit
+	dw 0x0000	; Base
+	db 0x00 	; Base
+	db 0b10011010	; Access byte
+	db 0b11001111	; Flags
+	db 0x00		; Base
+
+gdt_end:
+gdt_descriptor:
+	dw gdt_end - gdt_start - 1 ; size - 1
+	dd gdt_start
 msg: db "hello world", 0
 
 times 510 - ($ - $$) db 0
